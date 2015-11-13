6cfd05847be96a37bd81c60abdf408f708d9e34c
usi <none@none>
2015-11-13 14:20:22 +0100

    * Weitere arbeiten an Angular Integration für Optionen
    --HG--
    branch : Angular-Optionen

---
+++ data/options/angular_functions.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/* global angular, self */
+ 
+var usiOptions = angular.module('usiOptions', ["mobile-angular-ui"]);
+
+usiOptions.controller("ListUserScripts", ["$scope" ,function ListUserScripts($scope){
+	
+	$scope.all_userscripts = [];
+	// Liste alle verfügbaren Skripte auf
+		self.port.on("list-all-scripts", function (all_scripts) {
+			$scope.all_userscripts = all_scripts;
+			
+			console.log("$scope.all_userscripts");
+			console.log($scope.all_userscripts);
+		});
+	
+	
+}]);
