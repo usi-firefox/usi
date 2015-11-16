44de0e685af53d6306dc939728f34878e00b8289
usi <none@none>
2015-11-16 22:33:39 +0100

    * Userscript Auflistung nun mittels Angular
    --HG--
    branch : Angular-Optionen

--- data/options/angular_functions.js
+++ data/options/angular_functions.js
@@ -1,24 +1,55 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+"use strict";
 
-
-/* global angular, self */
+/* global angular, self, directive, basic_helper */
  
 var usiOptions = angular.module('usiOptions', ["mobile-angular-ui"]);
 
-usiOptions.controller("ListUserScripts", ["$scope" ,function ListUserScripts($scope){
-	
-	$scope.all_userscripts = [];
-	// Liste alle verfügbaren Skripte auf
-		self.port.on("list-all-scripts", function (all_scripts) {
-			$scope.all_userscripts = all_scripts;
-			
-			console.log("$scope.all_userscripts");
-			console.log($scope.all_userscripts);
-		});
-	
-}]);
+/************************************************************************
+ ************************* Übersetzungen holen **************************
+ ************************************************************************/
+var lang = self.options.language;
 
+usiOptions.controller("ListUserScripts", ["$scope", "$rootScope" , function ListUserScripts($scope, $rootScope){
+	// Var init...
+	$scope.all_userscripts = {};
+	$scope.lang = self.options.language;
+	
+	
+	$scope.request_for_reload = function(){
+		console.log("request_for_reload: aufgerufen ....");
+		
+		self.port.emit("request-for---list-all-scripts", false);
+	};
+	
+	$scope.toggleActivation = function (userscript){
+		// aktiviere oder deaktiviere dieses Userscript!
+		self.port.emit("toggle-userscript-state", userscript.id);
+	};
+	
+	$scope.delete = function (userscript){
+		// das Skript mit der ID löschen!
+		if (!basic_helper.empty(userscript.id)) {
+			//zusätzliche Abfrage
+			if(window.confirm( lang.want_to_delete_this_userscript_1 + userscript.id + lang.want_to_delete_this_userscript_2)){
+				
+				// @todo erstmal abschalten!!!
+				//self.port.emit("delete-script-by-id", userscript.id);
+			}
+		}
+	};
+	
+	// Events
+	self.port.on("list-all-scripts", function(data){
+		// Daten für alle Userscripts setzen
+		$scope.all_userscripts = data;
+	});
+	
+	
+	// Init
+	$scope.request_for_reload();
+	
+}]).directive("listuserscripts", function(){
+    return {
+		templateUrl : "directive/listuserscripts.html"
+    };
+});
