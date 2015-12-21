6d7f14639e15dd34da66f71703847c912b93451f
usi <none@none>
2015-12-21 14:34:31 +0100

    * weitere Aufräum arbeiten
    --HG--
    branch : Angular-Optionen

--- data/userscript-get.js
+++ data/userscript-get.js
@@ -13,18 +13,14 @@ self.port.on("active", function (status) {
 	// wird über die Konfigurationsvariable gesetzt
 	if (status === true) {
 
-// frage ob das Skript heruntergeladen werden soll
+		// frage ob das Skript heruntergeladen werden soll
 		if (window.confirm(lang.should_usi_import_this_userscript)) {
-//if (window.confirm("Soll dieses UserScript von USI geladen und aktiviert werden?")) {
 			// sende die aktuelle URL an das ADD-ON
 			self.port.emit("new-usi-script_url---call", {script_url: window.location.href});
 		}
 
-// Falls ein identisches Userscript gefunden wurde!
+		// Falls ein identisches Userscript gefunden wurde!
 		self.port.on("same-userscript-was-found", function (userscript_infos) {
-
-//	if (window.confirm("Ein Userscript mit den gleichen Einstellungen (usi-id: '" + userscript_infos.id + "') \n\
-//wurde gefunden, möchtest du es aktualisieren?")) {
 			if (window.confirm(lang.same_userscript_was_found_ask_update_it_1 + userscript_infos.id + lang.same_userscript_was_found_ask_update_it_2)) {
 				// Dieses Skript wird nun aktualisiert! userscript_infos = {id : id , userscript: userscript}
 				self.port.emit("override-same-userscript", userscript_infos);
