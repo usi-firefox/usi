408a0c52f8f9f065e468a3c7a16e72e18f015e08
usi <none@none>
2016-02-21 22:08:06 +0100

    * Komplette Restrukturierung des page_injection_helper! * Tabs durch Leerzeichen ersetzt (Beispiel Userscripte) * chome.manifest und options_fennec.xul wieder entfernt

--- data/options/example/en-example.user.js
+++ data/options/example/en-example.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name			{Userscript name}
-// @namespace	{e.g. your website}
-// @author		{your name}
-// @description		{Userscript description}
-// @include			*			
+// @name           {Userscript name}
+// @namespace      {e.g. your website}
+// @author         {your name}
+// @description    {Userscript description}
+// @include        *			
 //		https://developer.mozilla.org/en-US/Add-ons/SDK/Low-Level_APIs/util_match-pattern
 //		
-// @clean-include		(true || false) if it's > true < , USI will not change any of your @includes
-// @info				{more informations ...}
-// @updateURL		{USI can update your Userscript, if the version number has changed}
-// @run-at			(document-end || document-start || document-ready)
-// @include-jquery		(true || false)
-// @use-greasemonkey	(true || false)
-// @version			1.0.0
+// @clean-include       (true || false)
+// if it's > true < , USI will not change any of your @includes
+// 
+// @info                {more informations ...}
+// @updateURL           {USI can update your Userscript, if the version number has changed}
+// @run-at              (document-end || document-start || document-ready)
+// @include-jquery      (true || false)
+// @use-greasemonkey    (true || false)
+// @version             1.0.0
 // ==/UserScript==
 
 // WARNING: If you save this Userscript, it will be immediately active
