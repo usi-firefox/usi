39d4e71fefdc5fbce966922e68726f1862961f64
usi <none@none>
2016-08-13 11:24:35 +0200

    * Config Übersicht sollte nun erstmal grundlegend laufen
    --HG--
    branch : Angular-entfernen

--- data/options/js/controller/userscript/config.js
+++ data/options/js/controller/userscript/config.js
@@ -10,43 +10,84 @@ function userscript_config_class (){
 		
 		,after_rendering : function(){
 			
+			var __change_switch_option = this.__change_switch_option;
+			
+			var initial_requests_done = 4;
+			// Verstecke zunächst alle Labels
+			// registriere alle Events für die States
+			self.port.emit("USI-BACKEND:register-all-change-states");
+			
+			// Setze die gesetzen Einstellungen für die Buttons --- START
+			self.port.on("USI-BACKEND:options_always_activate_greasemonkey",function(state){
+				jQuery("#usi-config-change-options-always-activate-greasemonkey").prop("checked", state);
+				__change_switch_option("usi-config-change-options-always-activate-greasemonkey");
+				jQuery(document).trigger("all-intial-requests-done",--initial_requests_done);
+			});
+			self.port.on("USI-BACKEND:ExternalScriptLoadQuestion",function(state){
+				jQuery("#usi-config-change-enable-external-script-load-question").prop("checked", state);
+				__change_switch_option("usi-config-change-enable-external-script-load-question");
+				jQuery(document).trigger("all-intial-requests-done",--initial_requests_done);
+			});
+			self.port.on("USI-BACKEND:OldUsiIncludeBehavior",function(state){
+				jQuery("#usi-config-change-old-usi-include-behavior").prop("checked", state);
+				__change_switch_option("usi-config-change-old-usi-include-behavior");
+				jQuery(document).trigger("all-intial-requests-done",--initial_requests_done);
+			});
+			self.port.on("USI-BACKEND:highlightjs-activation-state",function(state){
+				jQuery("#usi-config-change-options-activate-highlightjs").prop("checked", state);
+				__change_switch_option("usi-config-change-options-activate-highlightjs");
+				
+				jQuery(document).trigger("all-intial-requests-done",--initial_requests_done);
+			});
+			
+			jQuery("#usi-config-change-complete-export---true").hide();
+			
+			// Setze die gesetzen Einstellungen für die Buttons --- END
+			
+			
 			// Button Events --- START
 			jQuery("#usi-config-delete-all").on("click", this.deleteAll );
 			jQuery("#usi-config-check-for-updates").on("click", this.checkForUpdates );
 			jQuery("#usi-config-export-all").on("click", this.exportAll );
 			
-			var __change_switch_option = this.__change_switch_option;
-			
-			jQuery("#usi-config-change-old-usi-include-behavior").on("change", function(){
-				__change_switch_option(this.id);
-				self.port.emit("USI-BACKEND:OldUsiIncludeBehavior-change", jQuery(this).prop("checked"));
-			});
-			jQuery("#usi-config-change-enable-external-script-load-question").on("change", function(){
-				__change_switch_option(this.id);
-				self.port.emit("USI-BACKEND:ExternalScriptLoadQuestion-change",  jQuery(this).prop("checked"));
-			});
-			jQuery("#usi-config-change-options-activate-highlightjs").on("change", function(){
-				// ändert den Aktivierungs Status
-				__change_switch_option(this.id);
-				self.port.emit("USI-BACKEND:highlightjs-activation-state-change",  jQuery(this).prop("checked"));
-			});
-			jQuery("#usi-config-change-options-always-activate-greasemonkey").on("change", function(){
-				// Aktiviert Greasemonkey Funktionen immer, egal ob @use-greasemonkey gesetzt wurde oder nicht
-				__change_switch_option(this.id);
-				self.port.emit("USI-BACKEND:options_always_activate_greasemonkey-change",  jQuery(this).prop("checked"));
-			});
-			
-			jQuery("#usi-config-change-complete-export").on("change", function(){
-				__change_switch_option(this.id);
+			// Sobald der Counter auf 0 steht, 
+			jQuery(document).on("all-intial-requests-done", function(evt, counter){
+
+				if(counter === 0){
+					// Die Buttons mittels switchery_controller anpassen
+					switchery_controller.run();
+
+					// Switch Events behandeln
+					jQuery("#usi-config-change-old-usi-include-behavior").on("change", function(){
+						__change_switch_option(this.id);
+						self.port.emit("USI-BACKEND:OldUsiIncludeBehavior-change", jQuery(this).prop("checked"));
+					});
+					jQuery("#usi-config-change-enable-external-script-load-question").on("change", function(){
+						__change_switch_option(this.id);
+						self.port.emit("USI-BACKEND:ExternalScriptLoadQuestion-change",  jQuery(this).prop("checked"));
+					});
+					jQuery("#usi-config-change-options-activate-highlightjs").on("change", function(){
+						// ändert den Aktivierungs Status
+						__change_switch_option(this.id);
+						self.port.emit("USI-BACKEND:highlightjs-activation-state-change",  jQuery(this).prop("checked"));
+					});
+					jQuery("#usi-config-change-options-always-activate-greasemonkey").on("change", function(){
+						// Aktiviert Greasemonkey Funktionen immer, egal ob @use-greasemonkey gesetzt wurde oder nicht
+						__change_switch_option(this.id);
+						self.port.emit("USI-BACKEND:options_always_activate_greasemonkey-change",  jQuery(this).prop("checked"));
+					});
+
+					jQuery("#usi-config-change-complete-export").on("change", function(){
+						__change_switch_option(this.id);
+					});
+				}
 			});
 			// Button Events --- END
 			
-			// Die Buttons mittels switchery_controller anpassen
-			switchery_controller.run();
 			
 		}
 		
-		, __change_switch_option : function(id, callback){
+		, __change_switch_option : function(id){
 			id = "#" + id; // für jQuery
 			var state = jQuery(id).prop("checked");
 			
@@ -56,10 +97,6 @@ function userscript_config_class (){
 			}else{
 				jQuery(id + "---true").hide();
 				jQuery(id + "---false").fadeIn();
-			}
-			
-			if(typeof callback === "function"){
-				callback();
 			}
 			
 		}
