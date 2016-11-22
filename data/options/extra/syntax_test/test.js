cc20b64bb956127dc65e7476d6783baa0a8e6707
usi <none@none>
2016-11-22 16:01:22 +0100

    * Bugfix

--- data/options/extra/syntax_test/test.js
+++ data/options/extra/syntax_test/test.js
@@ -1,20 +1,26 @@
 function appendScript(userscript) {
+    var newscript, newscript_textnode, userscript_lines;
+    
     // Script Darstellung --- START
     newscript = document.createElement("div");
     
-    var userscript_lines = userscript.split(/\n/g);
-    for (var i in userscript_lines){
-        // Zeilennummer hinzufügen
-        userscript_lines[i] = i + ": " + userscript_lines[i];
+    userscript_lines = userscript.split(/\n/g);
+    for (var i in userscript_lines) {
+        // Zeilennummer hinzufügen und Inhalt hinzufügen
+        newscript.appendChild(document.createTextNode(i + ": " + userscript_lines[i]));
+        // Zeilenumbruch hinzufügen
+        newscript.appendChild(document.createElement("br"));
     }
-   
-    newscript.innerHTML = userscript_lines.join("<br />");
+    
     document.body.appendChild(newscript);
     // Script Darstellung --- END
-    
-    
+
+
     // Skript ausführen, für den Syntax Check
     newscript = document.createElement("script");
-    newscript.innerHTML = userscript;
+    newscript_textnode = document.createTextNode(userscript);
+    
+    newscript.appendChild(newscript_textnode);
+
     document.body.appendChild(newscript);
 }
