250a36a5dcb18faf35bd23421eb0cf34d41e7d4b
usi <none@none>
2017-10-20 18:38:30 +0200

    * Möglichkeit zum Ersetzen für (Lang)Textblöcke hinzugefügt
    --HG--
    branch : WebExtension

--- gui/options/js/controller/language.js
+++ gui/options/js/controller/language.js
@@ -9,10 +9,15 @@ var language_controller = (function language_class() {
         lang: browser.i18n,
         replace_in_DOM: function () {
             jQuery("[data-usi-lang]").each(function (idx, element) {
-                // füge die Übersetzung als ersten Textknoten ein
-                jQuery(element).prepend(
-                    lang.getMessage(jQuery(element).attr("data-usi-lang"))
-                    );
+                let message = this.lang().getMessage(jQuery(element).attr("data-usi-lang"));
+                if (jQuery(element).attr("data-usi-lang-replace") === "true") {
+                    // ersetze den Inhalt
+                    jQuery(element).text(message);
+                    jQuery(element).removeAttr("data-usi-lang-replace");
+                } else {
+                    // füge die Übersetzung als ersten Textknoten ein
+                    jQuery(element).prepend(message);
+                }
                 // entferne das Attribut
                 jQuery(element).removeAttr("data-usi-lang");
             });
